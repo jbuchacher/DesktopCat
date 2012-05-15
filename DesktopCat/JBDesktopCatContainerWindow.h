53321822fe9e22f7989d28b75c7f0686093ea18b
Joshua Buchacher <jbuchacher@me.com>
2012-05-14 21:26:58 -0700

    added NSWindow subclass DesktopCatContainerWindow

---
+++ DesktopCat/JBDesktopCatContainerWindow.h
@@ -0,0 +1,13 @@
+//
+//  JBDesktopCatContainerWindow.h
+//  DesktopCat
+//
+//  Created by Joshua Buchacher on 5/14/12.
+//  Copyright (c) 2012 Joshua Buchacher. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JBDesktopCatContainerWindow : NSWindow
+
+@end
---
+++ DesktopCat/JBDesktopCatContainerWindow.m
@@ -0,0 +1,13 @@
+//
+//  JBDesktopCatContainerWindow.m
+//  DesktopCat
+//
+//  Created by Joshua Buchacher on 5/14/12.
+//  Copyright (c) 2012 Joshua Buchacher. All rights reserved.
+//
+
+#import "JBDesktopCatContainerWindow.h"
+
+@implementation JBDesktopCatContainerWindow
+
+@end
