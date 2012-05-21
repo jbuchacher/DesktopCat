e4832f2901009c5dd8425a5c1c21ddc7af38dd24
Joshua Buchacher <jbuchacher@me.com>
2012-05-20 20:07:10 -0700

    Wired up hierarchy of le view/window. Added some animation stubs

--- DesktopCat/JBDesktopCatController.h
+++ DesktopCat/JBDesktopCatController.h
@@ -7,7 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Quartz/Quartz.h>
+#import "JBDesktopCatContainerWindow.h"
+#import "JBDesktopCat.h"
 
 @interface JBDesktopCatController : NSObject
+
+@property (strong, nonatomic) JBDesktopCatContainerWindow *kittyCatWindow;
+@property (strong, nonatomic) JBDesktopCat *desktopCat;
+
+- (void) enterCat;
 
 @end
--- DesktopCat/JBDesktopCatController.m
+++ DesktopCat/JBDesktopCatController.m
@@ -7,7 +7,71 @@
 //
 
 #import "JBDesktopCatController.h"
+#import <Quartz/Quartz.h>
 
 @implementation JBDesktopCatController
+
+@synthesize kittyCatWindow = _kittyCatWindow;
+@synthesize desktopCat = _desktopCat;
+
+- (id) init
+{
+    if (self = [super init])
+    {
+        _kittyCatWindow = [JBDesktopCatContainerWindow catWindow];    // Get a cat container window.
+        _desktopCat = [[JBDesktopCat alloc]initWithFrame: _kittyCatWindow.frame];
+        [_desktopCat setWantsLayer:TRUE];
+        [_desktopCat.layer setBackgroundColor: CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1.0)];
+        [_kittyCatWindow setContentView: _desktopCat];
+    }
+    return self;
+}
+
+- (void) enterCat
+{
+    [_kittyCatWindow makeKeyAndOrderFront: _kittyCatWindow];    // Display a cat
+    [self setExpectationForCatToWalkRight];
+}
+
+// Animation, expectations of movement of cat (they don't take orders.)
+
+- (void) setExpectationForCatToWalkRight
+{
+
+    NSDictionary *animations = [NSDictionary dictionaryWithObject: [self desktopCatWalkingNormal] forKey:@"frameOrigin"];
+    [_desktopCat setAnimations: animations];
+    NSPoint newOrigin = NSMakePoint((_desktopCat.frame.origin.x + 200), 0.0);
+    [_desktopCat.animator setFrameOrigin: newOrigin];
+}
+
+- (void) setExpectationForCatToWalkLeft
+{
+    
+}
+
+// Returned animations
+
+- (CAKeyframeAnimation *) desktopCatWalkingNormal
+{
+        CAKeyframeAnimation *desktopCatWalkingNormal = [CAKeyframeAnimation animation];
+        desktopCatWalkingNormal.duration = 5.0f;
+        desktopCatWalkingNormal.values = [NSArray arrayWithObjects: [NSNumber numberWithFloat:0.0f], [NSNumber numberWithFloat:50.0f], [NSNumber numberWithFloat:100.0f], [NSNumber numberWithFloat:150.0f], [NSNumber numberWithFloat:200.0f], nil];
+        desktopCatWalkingNormal.timingFunction = [CAMediaTimingFunction functionWithName: kCAMediaTimingFunctionLinear];
+        desktopCatWalkingNormal.calculationMode = kCAAnimationLinear;   
+
+    return desktopCatWalkingNormal;
+}
+
+- (CAKeyframeAnimation *) desktopCatTurningAround
+{
+    CAKeyframeAnimation *desktopCatTurningAround = [CAKeyframeAnimation animation];
+    desktopCatTurningAround.duration = 1.0f;
+    desktopCatTurningAround.timingFunction = [CAMediaTimingFunction functionWithName: kCAMediaTimingFunctionLinear];
+    desktopCatTurningAround.calculationMode = kCAAnimationLinear;
+    
+    return desktopCatTurningAround;
+}
+
+
 
 @end
