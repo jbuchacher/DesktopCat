7c359caec1cee12ec822b47171f55468993ad293
Joshua Buchacher <jbuchacher@me.com>
2012-05-28 09:12:08 -1000

    implemented dirty cat head composite for settings view

--- DesktopCat/JBDesktopCatSettingsView.m
+++ DesktopCat/JBDesktopCatSettingsView.m
@@ -12,8 +12,21 @@
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    [[NSColor blackColor] set];
+    [[NSColor clearColor] set];
     NSRectFill([self frame]);
+    
+    [[NSColor blackColor] set];
+    
+    CGContextRef ctx = [[NSGraphicsContext currentContext] graphicsPort];
+    CGContextSetRGBFillColor(ctx, 1.0, 0.0, 0.0, 0.75);
+    
+    //CGRect catHeadRect = CGRectMake(0.0, 0.0, self.window.frame.size.width, self.window.frame.size.height);
+    
+    NSImage *img = [NSImage imageNamed:@"CatHeadComposite"];
+    [img compositeToPoint:NSMakePoint(0.0, 0.0) operation:NSCompositeSourceOver];
+    [self.window setHasShadow: NO];
+    [self.window setHasShadow: YES];
+    
 }
 
 @end
